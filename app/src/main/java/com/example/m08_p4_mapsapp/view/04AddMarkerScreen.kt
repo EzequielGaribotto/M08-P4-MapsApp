5467559fe37c1217a57221a2190db7f1c4b85e8d
EzequielGaribotto <ezequiel.garibotto.7e7@itb.cat>
2024-03-14 12:26:01 +0100

    geo permission fix

--- app/src/main/java/com/example/m08_p4_mapsapp/view/04AddMarkerScreen.kt
+++ app/src/main/java/com/example/m08_p4_mapsapp/view/04AddMarkerScreen.kt
@@ -1,12 +1,7 @@
 package com.example.m08_p4_mapsapp.view
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
 import com.example.m08_p4_mapsapp.AddMarkerContent
 import com.example.m08_p4_mapsapp.viewmodel.APIViewModel
 
